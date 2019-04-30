998377b8cd668e1287086e27016c27c8c002de30
marcelbuesing <buesing.marcel@googlemail.com>
2019-05-01 00:07:20 +0200

    Async await demo_stream

--- dbcc/examples/demo_stream.rs
+++ dbcc/examples/demo_stream.rs
@@ -1,39 +1,50 @@
-/// If you are using Rust 2018 no `external crate byteorder;` is necessary
-// The `gen` module exists because `cargo test` fails
-// if `j1939.rs` is directly in the examples folder
-// (since it's treated like an example and a `main` is expected).
-// Usually you could directly use `mod j1939;` here.
+#![feature(async_await, await_macro)]
+
+//! If you are using Rust 2018 no `external crate byteorder;` is necessary
+//! The `gen` module exists because `cargo test` fails
+//! if `j1939.rs` is directly in the examples folder
+//! (since it's treated like an example and a `main` is expected).
+//! Usually you could directly use `mod j1939;` here.
+//! T
+//!
+//! Hazard Lamps Flashing
+//! ```
+//! cansend vcan0 0CFDCCFE#11111111111111;
+//! ```
+//!
+//! ```
+//!cansend vcan0 0CFDCCFE#00000000000000;
+//! ```
+//! Hazard Lights To Be Off
+
 mod gen;
 
 /// Generated module
 use crate::gen::j1939;
 
-use futures::future::Future;
-use futures::stream::Stream;
+use futures_util::stream::StreamExt;
 use std::io;
 use std::time::Duration;
-use tokio;
 
-fn main() -> io::Result<()> {
+
+#[runtime::main]
+async fn main() -> io::Result<()> {
     let ival = Duration::from_secs(0);
+    let mut oel_stream = j1939::Oel::stream("vcan0", &ival, &ival)?;
+    let oel = await!(oel_stream.next()).expect("No next value")?;
 
-    let f = j1939::Oel::stream("vcan0", &ival, &ival)?.for_each(|oel| {
-        // Signal indicates the selected position of the operator's hazard light switch.
-        match oel.hazardlightswitch() {
-            j1939::HazardLightSwitch2365443326::HazardLampsToBeFlashing => {
-                println!("Hazard Lamps To Be Flashing")
-            }
-            j1939::HazardLightSwitch2365443326::HazardLampsToBeOff => {
-                println!("Hazard Lamps To Be Off")
-            }
-            j1939::HazardLightSwitch2365443326::NotAvailable => println!("Not available"),
-            j1939::HazardLightSwitch2365443326::Error => println!("Error"),
-            j1939::HazardLightSwitch2365443326::XValue(_) => unreachable!(),
+    // Signal indicates the selected position of the operator's hazard light switch.
+    match oel.hazardlightswitch() {
+        j1939::HazardLightSwitch2365443326::HazardLampsToBeFlashing => {
+            println!("Hazard Lamps To Be Flashing")
         }
-        Ok(())
-    });
-
-    tokio::run(f.map_err(|_| ()));
+        j1939::HazardLightSwitch2365443326::HazardLampsToBeOff => {
+            println!("Hazard Lamps To Be Off")
+        }
+        j1939::HazardLightSwitch2365443326::NotAvailable => println!("Not available"),
+        j1939::HazardLightSwitch2365443326::Error => println!("Error"),
+        j1939::HazardLightSwitch2365443326::XValue(_) => unreachable!(),
+    }
 
     Ok(())
 }
